374d6df69651631e8db9780fc49513250109fdb0
Term164 <jani.bangiev@gmail.com>
2023-01-04 10:52:11 +0100

    Reorganized some backend files + implemented chat

--- backend/public/src/prijava.js
+++ backend/public/src/prijava.js
@@ -2,18 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebas
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 import {getDatabase, set, ref, update} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
 import { getFirestore, doc, setDoc} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { getFirebaseConfig } from './firebase-config.js';
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDe8074PwjBQGoMrGvl0UU-RVL0dRTlye0",
-    authDomain: "exlibra-563bd.firebaseapp.com",
-    projectId: "exlibra-563bd",
-    storageBucket: "exlibra-563bd.appspot.com",
-    messagingSenderId: "60246911702",
-    appId: "1:60246911702:web:c6dfc71fbe0db8de1fb777"
-  };
+  const firebaseConfig = getFirebaseConfig();
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
