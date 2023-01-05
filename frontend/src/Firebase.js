b4591e0cc484e074c562c6d3b0421aabd959d48d
Term164 <jani.bangiev@gmail.com>
2023-01-05 11:44:59 +0100

    Started to implement user based elements.

--- frontend/src/Firebase.js
+++ frontend/src/Firebase.js
@@ -5,7 +5,6 @@ import { getFirebaseConfig } from './firebase-config.js';
 
 const firebaseConfig = getFirebaseConfig();
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const firestore = getFirestore();
 
 
@@ -40,5 +39,52 @@ async function saveNewUserData(){
 
 }
 
+function isUserSignedIn() {
+    return !!getAuth().currentUser;
+}
 
-export {signInWithGoogle, signInDefault, registerUserDefault};
+function getUserName() {
+    // TODO 5: Return the user's display name.
+    return getAuth().currentUser.displayName;
+}
+
+// Initiate firebase auth
+function initFirebaseAuth() {
+    onAuthStateChanged(getAuth(), authStateObserver);
+}
+
+function authStateObserver(user) {
+    if (user) {
+      // User is signed in!
+      // Get the signed-in user's profile pic and name.
+      var profilePicUrl = getProfilePicUrl();
+      var userName = getUserName();
+  
+      // Set the user's profile pic and name.
+      userPicElement.style.backgroundImage =
+        'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+      userNameElement.textContent = userName;
+  
+      // Show user's profile and sign-out button.
+      userNameElement.removeAttribute('hidden');
+      userPicElement.removeAttribute('hidden');
+      signOutButtonElement.removeAttribute('hidden');
+  
+      // Hide sign-in button.
+      signInButtonElement.setAttribute('hidden', 'true');
+  
+      // We save the Firebase Messaging Device token and enable notifications.
+      saveMessagingDeviceToken();
+    } else {
+      // User is signed out!
+      // Hide user's profile and sign-out button.
+      userNameElement.setAttribute('hidden', 'true');
+      userPicElement.setAttribute('hidden', 'true');
+      signOutButtonElement.setAttribute('hidden', 'true');
+  
+      // Show sign-in button.
+      signInButtonElement.removeAttribute('hidden');
+    }
+}
+
+export {signInWithGoogle, signInDefault, registerUserDefault, isUserSignedIn, getUserName};
