f43e6c6e11d9bc1482e044c5e3d468799e342f3e
LukaVolk <lv4594@student.uni-lj.si>
2023-01-05 22:35:58 +0100

    shranjevanje google racuna

--- backend/public/src/prijava.js
+++ backend/public/src/prijava.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 import { getFirestore, doc, setDoc, getDoc, getDocs, collection, query, where, limit} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 import { getFirebaseConfig } from './firebase-config.js';
   // TODO: Add SDKs for Firebase products that you want to use
@@ -12,30 +12,48 @@ import { getFirebaseConfig } from './firebase-config.js';
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const firestore = getFirestore(app);
-  
+  var db = firebase.firestore();
 
 
-  function dodajOsebo(uid, data){
-    setDoc(doc(firestore, 'users/' + uid), data);
-  }
+
+//dodajanje userja v firestore ob vpisu z google racunom
+async function dodajUser(user){
+  const userRef = doc(firestore, "users/" + user.uid);
+      const info = await getDoc(userRef);
+      if (info.exists()) {
+        console.log("Document data:", info.data());
+        window.location.href = "html/books.html";
+      } else {
+        db.collection("users").doc(user.uid).set({
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL
+        })
+        .then(function() {
+          console.log("User successfully saved to Firestore");
+          window.location.href = "html/books.html";
+        })
+        .catch(function(error) {
+          console.error("Error writing document: ", error);
+        });
+        console.log("Uporabnik dodan");
+      }
+      
+}
+
 
 googleL.addEventListener('click', (e) => {
     const provider = new firebase.auth.GoogleAuthProvider();
     
-    firebase.auth().signInWithPopup(provider)
-           .then(result => {
-                const user = result.user;
-                //const colRef = collection(firestore, "users");
-                console.log(user.email)
-                const docData = {
-                    displayName: user.displayName,
-                    email: user.email,
-                    photoURL: user.photoURL,
-                }
-                dodajOsebo(user.uid, docData)
-                document.location.href = "html/books.html";
-                
-            });             
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // The signed-in user info.
+      var user = result.user;
+      //prestaviStran(user);
+      dodajUser(user);
+      
+    }).catch(function(error) {
+      // Handle error
+    });        
 })
   
     
