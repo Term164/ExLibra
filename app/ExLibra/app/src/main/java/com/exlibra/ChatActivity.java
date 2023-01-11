1a908d384ed907c93e7ac7ae9353e4a81b5684c4
Tadej Sivić <tadej.sivic@gmail.com>
2023-01-11 21:13:36 +0100

    Fixed Chat and Creating Group Chats

--- app/ExLibra/app/src/main/java/com/exlibra/ChatActivity.java
+++ app/ExLibra/app/src/main/java/com/exlibra/ChatActivity.java
@@ -32,6 +32,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -41,6 +42,8 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
@@ -49,9 +52,9 @@ public class ChatActivity extends AppCompatActivity implements AdapterView.OnIte
     String TAG = "DEBUG";
     ListView list;
     ArrayList<String> groupIds = new ArrayList<>();
-    ArrayList<String> userIds = new ArrayList<>();
+    ArrayList<String> groupIds_ordered = new ArrayList<>();
     ArrayList<String> usernames = new ArrayList<>();
-    ArrayList<String> userImages = new ArrayList<String>();
+    HashMap<String, String> userId_userName = new HashMap<>();
 
     Context chatActivityContext;
 
@@ -62,19 +65,8 @@ public class ChatActivity extends AppCompatActivity implements AdapterView.OnIte
         list = findViewById(R.id.list);
         list.setOnItemClickListener(this);
 
-        /*
-        //initialise bottom navigation
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        navigation.setSelectedItemId(R.id.action_chat);
-        */
         chatActivityContext = this;
         getUserChatGroups();
-
-
-        //ImageView img = findViewById(R.id.imagetest);
-
-        //new DownloadImageTask(img).execute("https://firebasestorage.googleapis.com/v0/b/exlibra-563bd.appspot.com/o/pfp%2Fdefault.png?alt=media&token=aa0a928f-af17-4f5a-b835-cc53305ee0a4");
 
     }
 
@@ -83,54 +75,45 @@ public class ChatActivity extends AppCompatActivity implements AdapterView.OnIte
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
         final FirebaseUser usr = FirebaseAuth.getInstance().getCurrentUser();
         String myUserId = FirebaseAuth.getInstance().getUid();
-        CollectionReference cr = db.collection("/group");
+        CollectionReference cr = db.collection("/users");
         cr.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (!task.isSuccessful())
-                    return;
-
-                for (DocumentSnapshot doc: task.getResult().getDocuments()) {
-                    ArrayList<String> documentMembers = (ArrayList<String>) doc.get("members");
-                    if (documentMembers.size()<2) continue;
-                    //Log.e(TAG, "Members: "+documentMembers.get(0)+" "+documentMembers.get(1)+", i am "+myUserId);
-                    if (documentMembers.get(0).equals(myUserId) || (documentMembers.get(1).equals(myUserId))) {
-                        String gid = doc.getId();
-                        groupIds.add(gid);
-                        //Log.e(TAG, "Have group "+gid );
-                        String otherUserid = documentMembers.get(0).equals(myUserId) ? documentMembers.get(1) : documentMembers.get(0);
-                        userIds.add( otherUserid );
-                        //Log.e(TAG, "Added "+ otherUserid);
+                for (DocumentSnapshot doc : task.getResult().getDocuments()) {
+                    userId_userName.put(doc.getId(), doc.getString("username"));
+                    if (doc.getId().equals(usr.getUid())) { // that's me
+                        groupIds = (ArrayList<String>) doc.get("groups");
+                        Log.e(TAG, "my groups are "+groupIds);
                     }
                 }
-                //Log.e(TAG, "gonna start checking: "+userIds.size()  );
-                db.collection("/users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                CollectionReference cr = db.collection("/group");
+                cr.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         for (DocumentSnapshot doc : task.getResult().getDocuments()) {
-                            if (userIds.contains(doc.getId())) {
-                                usernames.add(doc.getString("username"));
-                                //userImages.add(doc.getString("profileurl"));
-                                //Log.d(TAG, "Added: "+doc.getString("username"));
+                            if (groupIds.contains(doc.getId())){
+                                groupIds_ordered.add(doc.getId());
+                                ArrayList<String> members = (ArrayList<String>) doc.get("members");
+                                Log.e(TAG, "found members: "+members);
+                                if (members.get(0).equals(usr.getUid())) // if it's me
+                                    usernames.add( userId_userName.get(members.get(1)) );
+                                else
+                                    usernames.add( userId_userName.get(members.get(0)) );
+                                Log.e(TAG, "added to usernames: "+usernames.get(usernames.size()-1) );
                             }
                         }
-
-
-                        String[] userImagesString = makeStringArray(userImages);
                         String[] usernamesString = makeStringArray(usernames);
                         ChatAdapter chatAdapter = new ChatAdapter(chatActivityContext, usernamesString);
-
                         Log.e(TAG, "adapter has "+chatAdapter.usernames.length+" usernames");
-
-                        list.setAdapter(chatAdapter);  // adapter
+                        list.setAdapter(chatAdapter);
                     }
                 });
-
             }
         });
-
     }
 
+    // Helper methods
     int[] makeIntArray(ArrayList<Integer> arrList){
         int[] arr = new int[arrList.size()];
         for (int i=0; i<arrList.size(); i++) {
@@ -150,17 +133,16 @@ public class ChatActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
         Log.e(TAG, "onItemClick: CLICKED "+position);
-        Log.d(TAG, "GID = "+groupIds.get(position));
+        Log.d(TAG, "GID = "+groupIds_ordered.get(position));
 
         Intent i = new Intent(ChatActivity.this, SpecificChatActivity.class);
-        i.putExtra("gid", groupIds.get(position));
+        i.putExtra("gid", groupIds_ordered.get(position));
         i.putExtra("hisUsername", usernames.get(position));
         startActivity(i);
     }
 }
 
 // Helper classes for displaying an image and username as a list item
-
 class ChatAdapter extends ArrayAdapter<String>{
     Context context;
     String[] usernames;
